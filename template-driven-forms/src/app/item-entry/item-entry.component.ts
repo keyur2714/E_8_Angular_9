31bf1765658ed96a739719aea7c2071716fbde60
keyur2714 <keyur.555kn@gmail.com>
2020-09-13 10:05:21 +0530

    Updated with validation and two way binding

--- template-driven-forms/src/app/item-entry/item-entry.component.ts
+++ template-driven-forms/src/app/item-entry/item-entry.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Item } from './item.model';
 import { NgForm } from '@angular/forms';
-
+declare var $:any;
 @Component({
   selector: 'app-item-entry',
   templateUrl: './item-entry.component.html',
@@ -15,7 +15,14 @@ export class ItemEntryComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.item.itemId = 1;
+    this.item.itemName = "TEST";
+    this.item.description = "Test Description";
+    this.item.itemPrice = 22;
+    $('[data-toggle="tooltip"]').tooltip();
   }
+
+  
 
   save(frm:NgForm) : void {
     console.log(frm);
@@ -23,8 +30,15 @@ export class ItemEntryComponent implements OnInit {
       this.isSubmitted = true;
       console.log(frm.value);
       console.log(frm.controls["itemName"].value);
-      this.item = frm.value;
+      // this.item = frm.value;
     }    
+  }
+
+  reset() : void {
+    this.item.itemId = 1;
+    this.item.itemName = "TEST";
+    this.item.description = "Test Description";
+    this.item.itemPrice = 22;
   }
 
 }
